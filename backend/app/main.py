0b5b5c0432add7e8652ef4e5b7eeb653532096fb
diyaanna06 <diyavarghese20june@gmail.com>
2026-06-23 00:10:18 +0530

    CORS fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,13 +17,13 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://next-step-beige-zeta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(
     auth_router,
